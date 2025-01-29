337573cb575afe12777d59b1d884c86f8a778a25
Alexandre Harrison <xandyharrison@gmail.com>
2025-01-28 21:43:24 -0300

    fixed handleStudentStatus handler on students-controller

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -8,35 +8,37 @@ const {
 } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    const { name, className, section, roll } = req.query;
-    const students = await getAllStudents({ name, className, section, roll });
-    res.json({ students });
-  });
-  
-  const handleAddStudent = asyncHandler(async (req, res) => {
-    const payload = req.body;
-    const message = await addNewStudent(payload);
-    res.json(message);
-  });
-  
-  const handleUpdateStudent = asyncHandler(async (req, res) => {
-    const { id: userId } = req.params;
-    const payload = req.body;
-    const message = await updateStudent({...payload, userId});
-    res.json(message);
-  });
-  
-  const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const student = await getStudentDetail(id);
-    res.json(student);
-  });
-  
-  const handleStudentStatus = asyncHandler(async (req, res) => {
-    const { userId, reviewerId, status } = req.body;
-    const message = await setStudentStatus({ userId, reviewerId, status });
-    res.json(message);
-  });
+  const { name, className, section, roll } = req.query;
+  const students = await getAllStudents({ name, className, section, roll });
+  res.json({ students });
+});
+
+const handleAddStudent = asyncHandler(async (req, res) => {
+  const payload = req.body;
+  const message = await addNewStudent(payload);
+  res.json(message);
+});
+
+const handleUpdateStudent = asyncHandler(async (req, res) => {
+  const { id: userId } = req.params;
+  const payload = req.body;
+  const message = await updateStudent({ ...payload, userId });
+  res.json(message);
+});
+
+const handleGetStudentDetail = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const student = await getStudentDetail(id);
+  res.json(student);
+});
+
+const handleStudentStatus = asyncHandler(async (req, res) => {
+  const { id: userId } = req.params;
+  const { status } = req.body;
+  const { id: reviewerId } = req.user;
+  const message = await setStudentStatus({ userId, reviewerId, status });
+  res.json(message);
+});
 
 module.exports = {
   handleGetAllStudents,
